f6a850c4ddd341ca8ae465371260cef7514c233a
unpuzzleapp <unpuzzleapp@gmail.com>
2021-06-03 23:02:46 +0530

    added dumb code

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const Auth = require('./Auth');
 router.use('/auth', Auth)
 router.get('/', function(req, res, next) {
   // const err = new Error('This is root route!') 
-  // err.status = 404;
+  // err.status = 404; 
   // next(err, req, res, next)
   res.status(200).send('Welcome to the MERN starter backend!');
   // res.status(200).send({ title: 'Express' });
