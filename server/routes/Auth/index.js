dcc9a0365894aca350b301103911e0c475c1c95d
unpuzzleapp <unpuzzleapp@gmail.com>
2021-06-03 15:04:14 +0530

    Added any role for middleware authentication

--- server/routes/Auth/index.js
+++ server/routes/Auth/index.js
@@ -4,9 +4,9 @@ const Roles = require('../../middlewares/AuthenticateRoles');
 const AuthController = require('../../controllers/Auth');
 
 router.post('/v1/login', AuthController.loginValidation(), AuthController.login);
-router.get('/v1/profile', Roles.Owner, AuthController.getProfile);
-router.put('/v1/profile', Roles.Owner, AuthController.updateProfile);
+router.get('/v1/profile', Roles.Any, AuthController.getProfile);
+router.put('/v1/profile', Roles.Any, AuthController.updateProfile);
 router.post('/v1/register', AuthController.saveValidation(), AuthController.register);
-router.get('/v1/', Roles.Admin, AuthController.getAllUser)
+router.get('/v1/', Roles.Admin, AuthController.getAllUser);
 
 module.exports = router;
