2f8fdae3f95a2c6881dae835d25849b69bb545b0
unpuzzleapp <unpuzzleapp@gmail.com>
2021-06-20 20:45:27 +0530

    Added env

--- client/src/constant/env.js
+++ client/src/constant/env.js
@@ -1,6 +1,6 @@
 export const DEFAULT_AXIOS_URL =
   process.env.REACT_APP_DEFAULT_AXIOS_URL ||
-  'https://us-east1-unpuzzle-ad500.cloudfunctions.net/api';
+  'https://unpuzzle-integrated-backend.herokuapp.com/api';
 
 export const DEFAULT_LOCAL_AXIOS_URL =
   'https://us-east1-unpuzzle-ad500.cloudfunctions.net/api';
